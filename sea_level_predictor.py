e3a147c244c581b28b90466c8871879d07eb65a8
Neuromanxer <96709916+Neuromanxer@users.noreply.github.com>
2024-06-20 16:59:26 -0400

    Add files via upload

---
+++ sea_level_predictor.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+
+def draw_plot():
+    # Read data from file
+    df = pd.read_csv('epa-sea-level.csv')
+
+    # Create scatter plot
+    plt.figure(figsize=(10, 6))
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], color='blue', label='Data')
+
+    # Create first line of best fit
+    slope, intercept, r_value, p_value, std_err = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    x_values = range(df['Year'].min(), 2051)
+    y_values = slope * x_values + intercept
+    plt.plot(x_values, y_values, color='red', label='Fit using all data')
+
+    # Create second line of best fit
+    df_recent = df[df['Year'] >= 2000]
+    slope_recent, intercept_recent, _, _, _ = linregress(df_recent['Year'], df_recent['CSIRO Adjusted Sea Level'])
+    x_values_recent = range(2000, 2051)
+    y_values_recent = slope_recent * x_values_recent + intercept_recent
+    plt.plot(x_values_recent, y_values_recent, color='green', label='Fit using data since 2000')
+
+    # Add labels and title
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
+    plt.legend()
+
+    # Save plot and return data for testing (DO NOT MODIFY)
+    plt.savefig('sea_level_plot.png')
+    return plt.gca()
+
+
